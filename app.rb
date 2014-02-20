0fcf64bc6f808f962b386b418b08e59e1fc0dea5
Charles Whittaker <charles.whittaker@gmail.com>
2014-02-19 19:13:00 -0600

    create new user functionality

--- app.rb
+++ app.rb
@@ -4,13 +4,29 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 
-get '/' do
-  "Hello world!"
-end
-
 class User < ActiveRecord::Base
-
-
 end
 
+get '/' do
+  @users = User.order('created_at DESC')
+  erb :"users/index"
+end
 
+get '/users/new' do
+  @user = User.new
+  erb :"users/new"
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    redirect "users/#{@user.id}"
+  else
+    erb :"users/new"
+  end
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :"users/show"
+end
